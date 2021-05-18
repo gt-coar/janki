f81d5ca0f649d32d9347d4e1f3ec4efedb863dd6
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2021-05-18 11:15:14 -0400

    start adding frontend sqlite

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 University System of Georgia and jupyterlab-gt-coar-theme contributors
+# Copyright (c) 2021 University System of Georgia and janki contributors
 # Distributed under the terms of the BSD-3-Clause License.
 
 import re
--- src/js/jupyterlab-sqlite3/src/index.ts
+++ src/js/jupyterlab-sqlite3/src/index.ts
@@ -1,6 +1,2 @@
 // Copyright (c) 2021 University System of Georgia and janki contributors
 // Distributed under the terms of the BSD-3-Clause License.
-
-export * from '@gt-coar/janki';
-export * from '@gt-coar/jupyterlab-sqlite3';
-export * from '@gt-coar/jupyterlab-libarchive';
